cbbf2df6a5ed92cb9de8a22c1f619caa2b867592
IronMan1405 <daksheshnankani1405@gmail.com>
2026-02-05 17:01:48 +0530

    wrote arm trajectory executor node

--- arm_trajectory_executor/arm_trajectory_executor.py
+++ arm_trajectory_executor/arm_trajectory_executor.py
@@ -1,3 +1,52 @@
 import rclpy
 from rclpy.node import Node
 from trajectory_msgs.msg import JointTrajectory
+from sensor_msgs.msg import JointState
+
+import time
+
+class TrajecotryExecutor(Node):
+    def __init__(self):
+        super().__init__("arm_trajectory_executor")
+		
+        self.traj_sub = self.create_subscription(JointTrajectory, '/arm/joint_trajectory', self.trajectory_callback, 10)
+        
+        self.state_pub = self.create_publishder(JointState, '/arm_joint_targets', 10)
+
+        self.executing = False
+
+        self.get_logger().info("Trajectory executor ready")
+
+    def trajectory_callback(self, traj: JointTrajectory):
+        if self.executing:
+            self.get_logger().warn("already executing trajectory, ignoring new one")
+            return
+
+        if not traj.points:
+            self.get_logger().warn("received empty trajectory")
+            return
+
+        self.executing = True
+        self.get_logger().info(f"Executing traj with {len(traj.points)} points")
+
+        for point in traj.points:
+            msg = JointState()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.name = traj.joint_names
+            msg.position = list(point.positions)
+
+            self.state_pub.publish(msg)
+            time.sleep(0.025)
+
+        self.executing = False
+        self.get_logger().info("Trajectory execution complete")
+
+def main():
+	rclpy.init()
+	node = TrajecotryExecutor()
+	rclpy.spin(node)
+	rclpy.destroy_node()
+	rclpy.shutdown()
+
+if __name__ == "__main__":
+	main()
